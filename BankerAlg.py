76c1ea276d7748aff0884f06852ff46623cd57a9
salma77 <salmaabdelfatah77@gmail.com>
2021-05-19 14:14:33 +0200

    edited BankerAlg.py, removed some comments

--- BankerAlg.py
+++ BankerAlg.py
@@ -128,8 +128,4 @@ while(on == 'y'):
     while on not in options:
         print("Please type 'y' for yes or 'n' for no")
         on = input() 
-        #0 1 0 2 0 0 3 0 2 2 1 1 0 0 2   
-        #7 5 3 3 2 2 9 0 2 2 2 2 4 3 3 
-
-#2 3 1 1 0 1 0 0 2 0 0 3 1 1 0 1 2 0 0 1 0 2 1 0 0 1 1 0 0 0 2 0 1 1 0    
-#2 3 1 1 0 3 2 1 2 3 0 1 2 3 0 5 4 2 1 0 1 4 4 0 0 3 2 0 5 0 2 6 1 1 0    
+            
